1d619b7ec87643374ddbea58ae619c3b4a25e532
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-10 12:15:30 +0300

    Completed Assignment. All tests passing.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+    self.split(/[.|!|?]/).reject(&:empty?).count
 
   end
 end
+
+str = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+puts str.count_sentences
